78845a31ade321ba8e13670d923a0d89da02a8fc
Oron <oronevron@gmail.com>
2017-04-15 18:26:43 +0300

    Get the messages array from the json file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function() {
     // Handler for .ready() called
+    var messages;
 
+    // Get the messages array from the json file
+    $.getJSON("data/data.json", function(messagesJson){
+        messages = messagesJson;
+    });
 });
