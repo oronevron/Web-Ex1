6fa8396426802c71b8180466d1f3bf5c276d5cde
Oron <oronevron@gmail.com>
2017-04-15 19:11:07 +0300

    Change setTemplate function to displayNextPage & Set the content and the images

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
+var messages;
+
 $(function() {
-    // Handler for .ready() called
-    var messages;
 
     // Get the messages array from the json file
     $.getJSON("data/data.json", function(messagesJson){
         messages = messagesJson;
 
-        setTemplate(messages[getNextMessageIndex()].template);
+        displayNextPage(messages[getNextMessageIndex()]);
     });
 });
 
@@ -14,6 +14,32 @@ function getNextMessageIndex() {
     return 0;
 }
 
-function setTemplate(templateId) {
-    $("#messagesContainer").load("./templates/" + templateId + ".html");
-}
+function displayNextPage(message) {
+
+    $("#messagesContainer").load("./templates/" + message.template + ".html", function() {
+
+        var content = $("#content");
+
+        for (var i in message.content) {
+            var contentMessage = message.content[i];
+
+            var p = $('<p />');
+            p.html(contentMessage);
+
+            content.append(p);
+        }
+
+        var images = $("#images");
+
+        var counter = new Date().getMilliseconds();
+
+        for (var i in message.images) {
+            var image = message.images[i];
+
+            var img = $('<img />');
+            img.attr('src', image + '?' + counter++);
+
+            images.append(img);
+        }
+    });
+};
