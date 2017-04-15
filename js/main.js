839c4d5c29e72e526ad830df27f304734426cbb9
Oron <oronevron@gmail.com>
2017-04-15 18:44:56 +0300

    Add setTemplate function

--- js/main.js
+++ js/main.js
@@ -5,5 +5,15 @@ $(function() {
     // Get the messages array from the json file
     $.getJSON("data/data.json", function(messagesJson){
         messages = messagesJson;
+
+        setTemplate(messages[getNextMessageIndex()].template);
     });
 });
+
+function getNextMessageIndex() {
+    return 0;
+}
+
+function setTemplate(templateId) {
+    $("#messagesContainer").load("./templates/" + templateId + ".html");
+}
